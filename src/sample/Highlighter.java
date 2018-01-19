48277ebb5e20ffea3d122eb55085d90412951719
Behrooz Fard <behrooz.fard@gmail.com>
2018-01-19 16:18:30 +0330

    a CodeArea to Displaying Tokens Added

--- src/sample/Highlighter.java
+++ src/sample/Highlighter.java
@@ -2,18 +2,21 @@ package sample;
 
 import cParser.CLexer;
 import cppParser.CPP14Lexer;
+import javaParser.JavaLexer;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.Token;
 import org.fxmisc.richtext.CodeArea;
 import pythonParser.Python3Lexer;
-import javaParser.JavaLexer;
+
 public class Highlighter {
     static String t;
-    static CodeArea codeArea = new CodeArea();
+    //    static CodeArea codeArea = new CodeArea();
+    static CodeArea highlighterCodeArea = (CodeArea) Main.scene.lookup("#highlighterCodeArea");
 
     public static void setString(String t) {
         Highlighter.t = t;
+        highlighterCodeArea.clear();
     }
 
 
@@ -29,18 +32,23 @@ public class Highlighter {
                 break;
         }
     }
+
     public static void JavaHighlight() {
         CharStream stream = new ANTLRInputStream(t);
         JavaLexer lexer = new JavaLexer(stream);
         Token token;
         while (true) {
             token = lexer.nextToken();
-            System.out.println(token);
+            highlighterCodeArea.appendText(
+                    "\ntext: " + token.getText() +
+                            "\t\ttype: " + token.getType() +
+                            "\t\tline: " + token.getLine() + "\n");
             // Exit in EOF
             if (token.getType() == -1)
                 break;
         }
     }
+
     public static void CHighlight() {
         CharStream stream = new ANTLRInputStream(t);
         CLexer lexer = new CLexer(stream);
@@ -53,6 +61,7 @@ public class Highlighter {
                 break;
         }
     }
+
     public static void CPPHighlight() {
         CharStream stream = new ANTLRInputStream(t);
         CPP14Lexer lexer = new CPP14Lexer(stream);
